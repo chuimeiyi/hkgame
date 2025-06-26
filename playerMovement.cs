d5d17cdc202cf1157f6770c4e7d2929d94ccdf25
Wong Wing Sum <demonsshadow721@gmail.com>
2025-06-26 16:13:30 +0800

    Revert "Add files via upload"
    This reverts commit 5dde37c098c92258fe555b1438d28a213aea2fd5.

--- playerMovement.cs
+++ playerMovement.cs
@@ -14,13 +14,12 @@ public class playerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     [Header("Jumping")]
-    public float jumpPower;
-    private float jumpTimeToApex = 6f;
-    public float jumpCutGravityMult = 2f;
-    public float jumpUpGravityMult = 0.5f;
-    public float PressedJumpTime;
-    [Range(0.01f,0.5f)]public float jumpInputBufferTime;
-    public bool IsJumping;
+    public float jumpPower = 20f;
+    private float jumpTimeToApex = 5f;
+    private bool _isJumpCut;
+    float jumpCutGravityMult = 40f;
+    public float LastOnGroundTime;
+    public float LastPressedJumpTime;
 
     [Header("GroundCheck")]
     public Transform groundCheckPos;
@@ -29,7 +28,7 @@ public class playerMovement : MonoBehaviour
 
     [Header("Gravity")]
     public float baseGravity = 1f;
-    public float maxFallSpeed = 10f;
+    public float maxFallSpeed = 15f;
     public float fallSpeedMultiplier = 1f;
 
     public KeyCode left;
@@ -52,7 +51,27 @@ public class playerMovement : MonoBehaviour
 
     void Update()
     {
-        PressedJumpTime -= Time.deltaTime;
+        
+        LastOnGroundTime -= Time.deltaTime;
+
+        LastPressedJumpTime -= Time.deltaTime;
+
+        if (LastOnGroundTime > 0) {
+            _isJumpCut = false;
+        }
+        else { 
+            _isJumpCut = true;
+        }
+
+        /*if (_isJumpCut)
+        {
+            rb.gravityScale(0);
+        }
+            
+            (_isJumpCut == true && rb.velocity.y < 0) {
+            rb.gravityScale = rb.gravityScale * jumpCutGravityMult;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, maxFallSpeed));
+        }*/
 
         Debug.Log("GroundCheckPods length: " + (groundCheckPods != null ? groundCheckPods.Length.ToString() : "null"));
 
@@ -93,37 +112,31 @@ public class playerMovement : MonoBehaviour
                 rb.velocity = new Vector2(0, rb.velocity.y);
             }
 
-            rb.gravityScale = baseGravity;
             if (Input.GetKeyDown(jump) && isGrounded)
             {
                 // rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-                OnJumpInput();
                 Jump();
-            }
-            if (Input.GetKeyUp(jump) && !isGrounded) {
-                rb.gravityScale = rb.gravityScale * jumpCutGravityMult;
-                //rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
             }
         }
 
-    }
+        Gravity();
 
-    private void Gravity()
-    {
-        if (rb.velocity.y < 0)
-        {
+    }
+    
+    private void Gravity() { 
+        if(rb.velocity.y < 0) {
             rb.gravityScale = baseGravity + fallSpeedMultiplier;
             rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
         }
-        else
-        {
-            rb.gravityScale = baseGravity;
+        else { 
+            rb.gravityScale = baseGravity;    
         }
     }
     private void Jump()
     {
         //Ensures we can't call Jump multiple times from one press
-        PressedJumpTime = 0;
+        LastPressedJumpTime = 0;
+        LastOnGroundTime = 0;
 
         jumpPower = Mathf.Abs(baseGravity) * jumpTimeToApex;
         float force = jumpPower;
@@ -133,11 +146,6 @@ public class playerMovement : MonoBehaviour
         rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
     }
 
- 
-    public void OnJumpInput()
-    {
-        PressedJumpTime = jumpInputBufferTime;
-    }
 
     private void OnDrawGizmosSelected()
     {
@@ -156,6 +164,6 @@ public class playerMovement : MonoBehaviour
 
     }
 
-
+    
 
 }
